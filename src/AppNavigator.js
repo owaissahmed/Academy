400e20e8ea032caa41ac7b6dde0bba8c6b4753b4
owaissahmed <104171912+owaissahmed@users.noreply.github.com>
2023-11-18 20:54:56 +0500

    create new screen HOME

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import First from './Screens/First';
 import Form from './Screens/Form';
+import Home from './Screens/Home';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 const Stack = createStackNavigator();
@@ -8,7 +9,12 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="First">
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
       <Stack.Screen
         name="First"
         component={First}
