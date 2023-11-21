995e238074be7c3d88f670ba8a0189a55b92ae6f
owaissahmed <104171912+owaissahmed@users.noreply.github.com>
2023-11-21 22:27:54 +0500

    try to change UI

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -16,6 +16,7 @@ import {
   responsiveFontSize,
   responsiveHeight,
   responsiveWidth,
+  responsiveScreenFontSize
 } from 'react-native-responsive-dimensions';
 import NetInfo from '@react-native-community/netinfo';
 const devicewidth = Dimensions.get('window').width;
@@ -53,50 +54,66 @@ export default function Home() {
         resizeMode="cover"
         style={styles.background}
         source={require('../Images/background.jpg')}>
-        <Animatable.View
-          duration={2000}
-          delay={250}
-          animation="fadeInUp"
-          style={styles.topHeadingView}>
-          <Animatable.Text
-            duration={2000}
-            delay={250}
-            style={styles.topHeading}>
-            ازھارالاسلام اکیڈمی
-          </Animatable.Text>
-          <Animatable.Text
-            duration={2000}
-            delay={250}
-            style={styles.subHeading}>
-            آن لائن دینی تعلیم کا مستند ادارہ
-          </Animatable.Text>
-        </Animatable.View>
-        <Animatable.View animation="fadeInUp" duration={2000} delay={400}>
-          <TouchableOpacity style={styles.button} onPress={Youtube}>
-            <Text style={styles.buttonText}>DARS-e-NIZAMI HELP DESK</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>SHORT COURSES</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>ONLINE TUTION</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>HOME TUTION</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>DARS-e-NIZAMI COURSE</Text>
-          </TouchableOpacity>
-        </Animatable.View>
-        <Animatable.View
-          animation="fadeInUp"
-          duration={2000}
-          delay={600}
-          style={styles.bottom}>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>ABOUT US</Text>
-          </TouchableOpacity>
-        </Animatable.View>
+        <View style={styles.submain}>
+        <View style={styles.rectangle}>
+        <Text allowFontScaling={false} style={styles.rectangletext}>
+        ازھارالاسلام اکیڈمی
+      </Text>
+        <Text allowFontScaling={false} style={styles.rectangletext_}>
+        آن لائن دینی تعلیم کا مستند ادارہ
+      </Text>
+        </View>
+      </View>
+      <View style={styles.squarediv}>
+        <TouchableOpacity >
+          <View style={styles.square}>
+          <Image style={styles.youtube} source={require('../Images/youtube.png')}/>
+            <Text allowFontScaling={false} style={styles.squaretext__}>
+            DARS-e-NIZAMI HELP DESK
+          </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={styles.square}>
+          <Image style={styles.books} source={require('../Images/books.png')}/>
+            <Text allowFontScaling={false} style={styles.squaretext}>
+            SHORT COURSES
+          </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.square}>
+          <Image style={styles.online} source={require('../Images/online.png')}/>
+            <Text allowFontScaling={false} style={styles.squaretext}>
+            ONLINE TUTION
+          </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={styles.square}>
+          <Image style={styles.home} source={require('../Images/home.png')}/>
+            <Text allowFontScaling={false} style={styles.squaretext}>
+            HOME TUTION
+          </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={styles.square}>
+          <Image style={styles.quran} source={require('../Images/quran.png')}/>
+            <Text allowFontScaling={false} style={styles.squaretext__}>
+            DARS-e-NIZAMI COURSE
+          </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={styles.square}>
+            <Image style={styles.info} source={require('../Images/info.png')}/>
+            <Text allowFontScaling={false} style={styles.squaretext}>
+            ABOUT US
+          </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
       </ImageBackground>
     </View>
   );
@@ -160,4 +177,167 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     letterSpacing: 0.25,
   },
+
+
+  squarediv: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    width: responsiveWidth(95),
+    // backgroundColor:'red',
+    marginTop:responsiveHeight(-6)
+        
+  },
+  submain: {
+    // height: responsiveHeight(8),
+    // width: responsiveWidth(90),
+    // marginBottom: responsiveHeight(3),
+    // alignItems: 'center',
+    // backgroundColor:'red'
+  },
+  rectangle: {
+    // borderColor: '#135229',
+    // borderWidth: 1.5,
+    // height: responsiveHeight(10),
+    // width: responsiveWidth(90),
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // borderRadius: 12,
+  },
+  rectangletext: {
+    fontFamily: 'mushaf',
+    textTransform: 'uppercase',
+    color: '#2e4c60',
+    fontSize: responsiveFontSize(8.5),
+    textAlign: 'center',
+    // marginTop: responsiveHeight(4),
+  },
+  rectangletext_: {
+    fontFamily: 'mushaf',
+    textTransform: 'uppercase',
+    color: '#2e4c60',
+    fontSize: responsiveFontSize(4.5),
+    textAlign: 'center',
+    marginTop: responsiveHeight(-3),
+  },
+  
+  squaretext: {
+    fontSize: responsiveScreenFontSize(2.35),
+    color: '#2e4c60',
+    textAlign: 'center',
+    fontFamily: 'good',
+    marginTop:responsiveHeight(1),
+  },
+  squaretext__: {
+    fontSize: responsiveScreenFontSize(2),
+    color: '#2e4c60',
+    textAlign: 'center',
+    fontFamily: 'good',
+    marginTop:responsiveHeight(1),
+// backgroundColor:'green',
+lineHeight:20
+    // paddingHorizontal: responsiveWidth(1),
+  },
+  square: {
+    marginTop: responsiveHeight(3),
+    borderColor: '#2e4c60',
+    borderWidth: 1.5,
+    height: responsiveHeight(19),
+    width: responsiveWidth(39),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+  //  alignContent:'flex-end',
+    marginHorizontal: responsiveWidth(3),
+  },
+  info:{
+    height: responsiveHeight(8),
+    width: responsiveWidth(10),
+    marginBottom:responsiveHeight(0.75),
+  //  marginVertical:responsiveHeight(1),
+    // backgroundColor:'silver'
+  },
+  quran:{
+    height: responsiveHeight(8),
+    width: responsiveWidth(24),
+    marginTop:responsiveHeight(1),
+    // marginVertical:responsiveHeight(1),
+    // backgroundColor:'silver'
+  },
+  online:{
+    height: responsiveHeight(9),
+    width: responsiveWidth(24),
+    marginBottom:responsiveHeight(0.5),
+    // marginVertical:responsiveHeight(1),
+    // backgroundColor:'silver'
+  },
+  home:{
+    height: responsiveHeight(9),
+    width: responsiveWidth(35),
+    marginTop:responsiveHeight(1),
+    // marginVertical:responsiveHeight(1),
+    // backgroundColor:'silver'
+  },
+  books:{
+    height: responsiveHeight(9),
+    width: responsiveWidth(20),
+    marginTop:responsiveHeight(1),
+    // marginVertical:responsiveHeight(1),
+    // backgroundColor:'silver'
+  },
+  youtube:{
+    height: responsiveHeight(9),
+    width: responsiveWidth(20),
+    marginTop:responsiveHeight(0.5),
+    // marginVertical:responsiveHeight(1),
+    // backgroundColor:'silver'
+  }
+
 });
+
+
+// <Animatable.View
+// duration={2000}
+// delay={250}
+// animation="fadeInUp"
+// style={styles.topHeadingView}>
+// <Animatable.Text
+//   duration={2000}
+//   delay={250}
+//   style={styles.topHeading}>
+//   ازھارالاسلام اکیڈمی
+// </Animatable.Text>
+// <Animatable.Text
+//   duration={2000}
+//   delay={250}
+//   style={styles.subHeading}>
+//   آن لائن دینی تعلیم کا مستند ادارہ
+// </Animatable.Text>
+// </Animatable.View>
+// <Animatable.View animation="fadeInUp" duration={2000} delay={400}>
+// <TouchableOpacity style={styles.button} onPress={Youtube}>
+//   <Text style={styles.buttonText}>DARS-e-NIZAMI HELP DESK</Text>
+// </TouchableOpacity>
+// <TouchableOpacity style={styles.button}>
+//   <Text style={styles.buttonText}>SHORT COURSES</Text>
+// </TouchableOpacity>
+// <TouchableOpacity style={styles.button}>
+//   <Text style={styles.buttonText}>ONLINE TUTION</Text>
+// </TouchableOpacity>
+// <TouchableOpacity style={styles.button}>
+//   <Text style={styles.buttonText}>HOME TUTION</Text>
+// </TouchableOpacity>
+// <TouchableOpacity style={styles.button}>
+//   <Text style={styles.buttonText}>DARS-e-NIZAMI COURSE</Text>
+// </TouchableOpacity>
+// </Animatable.View>
+// <Animatable.View
+// animation="fadeInUp"
+// duration={2000}
+// delay={600}
+// style={styles.bottom}>
+// <TouchableOpacity style={styles.button}>
+//   <Text style={styles.buttonText}>ABOUT US</Text>
+// </TouchableOpacity>
+// </Animatable.View>
