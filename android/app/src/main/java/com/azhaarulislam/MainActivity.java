b880761ccfec61c3a7a95273c48bb239a69c69e3
owaissahmed <104171912+owaissahmed@users.noreply.github.com>
2023-12-13 21:08:19 +0500

    app name change

--- android/app/src/main/java/com/azhaarulislam/MainActivity.java
+++ android/app/src/main/java/com/azhaarulislam/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "AzhaarulIslam";
+    return "Azhaar-ul-Islam Academy";
   }
 
   /**
