cdbfaf60616a05f1a5620b078f5f032c835fe184
owaissahmed <104171912+owaissahmed@users.noreply.github.com>
2023-12-22 22:03:22 +0500

    New Course add by Admin done

--- src/Screens/UpcomingCourses.js
+++ src/Screens/UpcomingCourses.js
@@ -1,76 +1,97 @@
 import {
-    View,
-    Text,
-    Dimensions,
-    StyleSheet,
-    TouchableOpacity,
-    TextInput,
-    FlatList,
-    Alert,
-  } from 'react-native';
-  import React, {useEffect, useState} from 'react';
-  import {
-    responsiveScreenFontSize,
-    responsiveWidth,
-  } from 'react-native-responsive-dimensions';
-  import {responsiveHeight} from 'react-native-responsive-dimensions';
-  import {responsiveFontSize} from 'react-native-responsive-dimensions';
-  import firestore from '@react-native-firebase/firestore';
-  const devicewidth = Dimensions.get('window').width;
-  const deviceheight = Dimensions.get('window').height;
-  
-  const UpcomingCourses = () => {
-    const [courses, setcourses] = useState([]);
-  
-    useEffect(() => {
-      const unsubscribe = firestore()
-        .collection('Upcoming Courses')
-        .onSnapshot(querySnapshot => {
-          const coursesData = [];
-          querySnapshot.forEach(documentSnapshot => {
-            coursesData.push({
-              id: documentSnapshot.id,
-              ...documentSnapshot.data(),
-            });
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+  ImageBackground,
+  Alert,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  responsiveScreenFontSize,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
+import {responsiveHeight} from 'react-native-responsive-dimensions';
+import {responsiveFontSize} from 'react-native-responsive-dimensions';
+import firestore from '@react-native-firebase/firestore';
+const devicewidth = Dimensions.get('window').width;
+const deviceheight = Dimensions.get('window').height;
+
+const UpcomingCourses = () => {
+  const [courses, setcourses] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('New Course')
+      .onSnapshot(querySnapshot => {
+        const coursesData = [];
+        querySnapshot.forEach(documentSnapshot => {
+          coursesData.push({
+            id: documentSnapshot.id,
+            ...documentSnapshot.data(),
           });
-          setcourses(coursesData);
         });
-  
-      return () => unsubscribe();
-    }, []);
-  
-  
-    return (
+        setcourses(coursesData);
+      });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <ImageBackground
+      resizeMode="cover"
+      style={styles.background}
+      source={require('../Images/background.jpg')}>
+      <View>
+        <Text allowFontScaling={false} style={styles.HeadingText}>
+          COURSES
+        </Text>
+      </View>
       <View style={styles.main}>
         {courses.length > 0 ? (
           <View>
-            <View style={styles.FlatListVIew}>
+            <View style={styles.Description_View}>
               <FlatList
                 data={courses}
                 renderItem={({item}) => (
-                  <TouchableOpacity
-                    style={styles.DataView}>
-                    <View style={styles.DataView}>
-                      <Text allowFontScaling={false} style={styles.Name}>
-                        Name: {item.Name}
+                  <>
+                    <View style={styles.V_P_View}>
+                      <Text allowFontScaling={false} style={styles.Coursename}>
+                        {item.CourseName}
                       </Text>
-                      <Text allowFontScaling={false} style={styles.Name}>
-                        Phone: {item.MobileNo}
-                      </Text>
-                      <Text allowFontScaling={false} style={styles.Name}>
-                        Status: {item.Status}
-                      </Text>
-                      <Text allowFontScaling={false} style={styles.Name}>
-                    Father Name: {item.Fathername}
-                    </Text>
-                    <Text allowFontScaling={false} style={styles.Name}>
-                    Jamia: {item.Jamia}
-                    </Text>
-                    <Text allowFontScaling={false} style={styles.Name}>
-                    City: {item.City}
-                    </Text>
+                      <View style={styles.D_F_View}>
+                        <Text
+                          allowFontScaling={false}
+                          style={styles.DescriptionText}>
+                          Duration : {item.Duration}
+                        </Text>
+                        <Text
+                          allowFontScaling={false}
+                          style={styles.DescriptionText}>
+                          Fees : {item.Fees}
+                        </Text>
+                      </View>
                     </View>
-                  </TouchableOpacity>
+                    <View style={styles.ButtonView}>
+                      <TouchableOpacity style={styles.Button}>
+                        <Text
+                          allowFontScaling={false}
+                          style={styles.ButtonText}>
+                          Demo Class
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity style={styles.Button}>
+                        <Text
+                          allowFontScaling={false}
+                          style={styles.ButtonText}>
+                          Addmission
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  </>
                 )}
                 keyExtractor={item => item.id}
               />
@@ -82,20 +103,27 @@ import {
           </Text>
         )}
       </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    main: {
-    backgroundColor: 'white',
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
     width: devicewidth,
     height: deviceheight,
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical:responsiveHeight(10)
+    justifyContent: 'space-evenly',
   },
+  // main: {
+  //   backgroundColor: 'white',
+  //   width: devicewidth,
+  //   height: deviceheight,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   paddingVertical: responsiveHeight(10),
+  // },
   FlatListVIew: {
-      width: responsiveWidth(90),
+    width: responsiveWidth(90),
   },
   DataView: {
     backgroundColor: '#135229',
@@ -106,6 +134,18 @@ import {
     textAlign: 'center',
     fontSize: responsiveFontSize(2.25),
   },
+  HeadingText: {
+    fontFamily: 'good',
+    color: '#2e4c60',
+    // backgroundColor: 'seagreen',
+    fontSize: responsiveFontSize(5),
+    textAlign: 'center',
+    // marginVertical:responsiveHeight(),
+    marginTop: responsiveHeight(3),
+    // marginBottom: responsiveHeight(2),
+    textTransform: 'uppercase',
+    letterSpacing: 2,
+  },
   Update: {
     backgroundColor: '#135229',
     borderRadius: 10,
@@ -114,21 +154,21 @@ import {
     color: 'white',
     textAlign: 'center',
     fontSize: responsiveFontSize(2.25),
-    marginBottom:responsiveHeight(-0.5),
+    marginBottom: responsiveHeight(-0.5),
   },
   Name: {
-      color: 'white',
-      fontSize: responsiveFontSize(2.25),
-      textAlign: 'center',
+    color: 'white',
+    fontSize: responsiveFontSize(2.25),
+    textAlign: 'center',
   },
   Phone: {
-      color: 'white',
-      fontSize: responsiveFontSize(2.25),
-      textAlign: 'center',
+    color: 'white',
+    fontSize: responsiveFontSize(2.25),
+    textAlign: 'center',
   },
   NoData: {
-      fontSize: responsiveFontSize(3),
-      color: 'red',
+    fontSize: responsiveFontSize(3),
+    color: 'red',
   },
   password: {
     borderRadius: 10,
@@ -142,7 +182,77 @@ import {
     color: 'black',
     height: responsiveHeight(5),
   },
+  Description_View: {
+    padding: 10,
+    marginHorizontal: responsiveWidth(4),
+    width: responsiveWidth(90),
+    backgroundColor: 'white',
+    borderColor: '#2e4c60',
+    borderWidth: 2,
+    height: responsiveHeight(20),
+    marginTop: responsiveHeight(-2.25),
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  V_P_View: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: responsiveWidth(1),
+    borderRadius: 8,
+  },
+  D_F_View: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: responsiveWidth(90),
+    // backgroundColor: 'red',
+    paddingHorizontal: responsiveWidth(1),
+    borderRadius: 8,
+  },
+  Coursename: {
+    color: '#2e4c60',
+    fontSize: responsiveScreenFontSize(4),
+    fontFamily: 'mushaf',
+    // fontWeight: 'bold',
+    // letterSpacing: 0.25,
+    marginVertical: responsiveHeight(-2),
+  },
+  DescriptionText: {
+    color: '#2e4c60',
+    fontSize: responsiveScreenFontSize(2.25),
+    marginHorizontal: responsiveWidth(4),
+    fontFamily: 'nunito',
+    fontWeight: 'bold',
+    // backgroundColor: 'lightgreen',
+    letterSpacing: 0.25,
+    marginTop: responsiveHeight(3),
+    // paddingVertical: responsiveHeight(0.75),
+    // paddingHorizontal: responsiveWidth(1.5),
+    // borderBottomRightRadius: 10,
+    // borderTopRightRadius: 10
+  },
+  ButtonView: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  Button: {
+    marginTop: responsiveHeight(1),
+    backgroundColor: '#2e4c60',
+    paddingVertical: responsiveHeight(0.75),
+    paddingHorizontal: responsiveWidth(3),
+    borderRadius: 8,
+  },
+  ButtonText: {
+    color: '#fff',
+    fontSize: responsiveScreenFontSize(2),
+    fontFamily: 'nunito',
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+  },
 });
 
-
-export default UpcomingCourses
+export default UpcomingCourses;
