bcfb5d234a00b09215a03bf840cfa5818fba12a7
owaissahmed <104171912+owaissahmed@users.noreply.github.com>
2023-12-26 21:23:06 +0500

    Styling Start For Account Page

--- src/Screens/UserAccount.js
+++ src/Screens/UserAccount.js
@@ -1,14 +1,136 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+  ImageBackground,
+  ActivityIndicator,
+  Alert,
+  Modal,
+  Image,
+  Linking,
+} from 'react-native';
+import PhoneInput from 'react-native-phone-number-input';
+import firestore from '@react-native-firebase/firestore';
+import firebase from '@react-native-firebase/app';
+import NetInfo from '@react-native-community/netinfo';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
+const devicewidth = Dimensions.get('window').width;
+const deviceheight = Dimensions.get('window').height;
+import {useRoute} from '@react-navigation/native';
+import {useAppContext} from './AppContext';
+import * as Animatable from 'react-native-animatable';
+import FlashMessage, {showMessage} from 'react-native-flash-message';
 
 const UserAccount = ({navigation}) => {
+  
+  const [value, setValue] = useState('');
+  const [formattedValue, setFormattedValue] = useState('');
+  const [country, setCountry] = useState('Pakistan');
+  const [countryCode, setCountryCode] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+  const phoneInput = useRef(null);
+
+  const handleOnCountryChange = country => {
+    setCountry(country);
+  };
+
+  function ww() {
+    console.log(formattedValue);
+  }
+
   return (
     <View>
-      <Text>UserAccount</Text>
+      <ImageBackground
+        resizeMode="cover"
+        style={styles.background}
+        source={require('../Images/background.jpg')}>
+        <View style={styles.main}>
+          <View>
+            <PhoneInput
+              textInputProps={{
+                placeholderTextColor: 'grey',
+              }}
+              containerStyle={{
+                width: responsiveWidth(80),
+                height: responsiveHeight(6),
+                marginTop: responsiveHeight(3),
+                borderColor: '#36454F',
+                borderWidth: 1.5,
+                backgroundColor: '#FBFCF8',
+              }}
+              flagButtonStyle={{
+                backgroundColor: '#FBFCF8',
+              }}
+              textInputStyle={{
+                height: responsiveHeight(6),
+                width: responsiveWidth(70),
+                color: '#36454F',
+                marginTop: responsiveHeight(0.2),
+                fontSize: responsiveFontSize(2),
+                textAlignVertical: 'center',
+              }}
+              codeTextStyle={{
+                color: '#36454F',
+                fontSize: responsiveFontSize(2),
+                height: responsiveHeight(7),
+                fontWeight: 'normal',
+                textAlignVertical: 'center',
+              }}
+              onChangeCountry={handleOnCountryChange}
+              ref={phoneInput}
+              defaultValue={value}
+              defaultCode="PK"
+              layout="first"
+              onChangeText={text => {
+                setValue(text);
+              }}
+              onChangeFormattedText={text => {
+                setFormattedValue(text);
+                setCountryCode(phoneInput.current?.getCountryCode() || '');
+              }}
+              countryPickerProps={{withAlphaFilter: true}}
+            />
+          </View>
+          <Text onPress={ww}>Enter Your Phone No. To See Your Requests </Text>
+        </View>
+      </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
-export default UserAccount
+export default UserAccount;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  background: {
+    width: devicewidth,
+    height: deviceheight,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  main:{
+    backgroundColor: 'red',
+    display:'flex',
+    alignItems: 'center',
+    // justifyContent: 'space-evenly',
+  },
+  login: {
+    height: responsiveHeight(6),
+    width: responsiveWidth(80),
+    backgroundColor: '#FBFCF8',
+    padding: 8,
+    borderColor: '#36454F',
+    color: '#36454F',
+    borderWidth: 1.5,
+    marginTop: responsiveHeight(2),
+    fontSize: responsiveFontSize(2),
+  },
+});
