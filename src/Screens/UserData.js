00f70ec56c331494ea96a727be062fc495ea6e99
owaissahmed <104171912+owaissahmed@users.noreply.github.com>
2023-12-29 21:11:46 +0500

    Respons & Fees showing by Ternary Operators

--- src/Screens/UserData.js
+++ src/Screens/UserData.js
@@ -1,130 +1,143 @@
 import {
-    View,
-    Text,
-    Image,
-    Dimensions,
-    StyleSheet,
-    TouchableOpacity,
-    ImageBackground,
-    FlatList,
-  } from 'react-native';
-  import React, {useEffect, useState} from 'react';
-  import {
-    responsiveScreenFontSize,
-    responsiveWidth,
-  } from 'react-native-responsive-dimensions';
-  import {responsiveHeight} from 'react-native-responsive-dimensions';
-  import {responsiveFontSize} from 'react-native-responsive-dimensions';
-  import firestore from '@react-native-firebase/firestore';
-  const devicewidth = Dimensions.get('window').width;
-  const deviceheight = Dimensions.get('window').height;
-  import * as Animatable from 'react-native-animatable';
-  import {useRoute} from '@react-navigation/native';
-  const UserData = ({navigation}) => {
-    const [online, setonline] = useState([]);
-  
-    useEffect(() => {
-      const unsubscribe = firestore()
-        .collection('users')
-        .where('Phone', '==', phoneNo)
-        .onSnapshot(querySnapshot => {
-          const onlineData = [];
-          querySnapshot.forEach(documentSnapshot => {
-            onlineData.push({
-              id: documentSnapshot.id,
-              ...documentSnapshot.data(),
-            });
-          });
-          setonline(onlineData);
-        });
-  
-      return () => unsubscribe();
-    }, []);
+  View,
+  Text,
+  Image,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  FlatList,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  responsiveScreenFontSize,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
+import {responsiveHeight} from 'react-native-responsive-dimensions';
+import {responsiveFontSize} from 'react-native-responsive-dimensions';
+import firestore from '@react-native-firebase/firestore';
+const devicewidth = Dimensions.get('window').width;
+const deviceheight = Dimensions.get('window').height;
+import * as Animatable from 'react-native-animatable';
+import {useRoute} from '@react-navigation/native';
+const UserData = ({navigation}) => {
+  const [online, setonline] = useState([]);
 
-    const route = useRoute();
-    const { phoneNo } = route.params;
-  
-    return (
-      <View>
-        <ImageBackground
-          resizeMode="cover"
-          style={styles.background}
-          source={require('../Images/background.jpg')}>
-          {online.length > 0 ? (
-            <View>
-              <View style={styles.FlatListVIew}>
-                <FlatList
-                  data={online}
-                  renderItem={({item}) => (
-                    <TouchableOpacity style={styles.DataView}>
-                      <View style={styles.DataView}>
-                        <Text allowFontScaling={false} style={styles.Name}>
-                          Name : {item.Name}
-                        </Text>
-                        <Text allowFontScaling={false} style={styles.Name}>
-                          Father Name : {item.Fathername}
-                        </Text>
-                        <Text allowFontScaling={false} style={styles.Name}>
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('users')
+      .where('Phone', '==', phoneNo)
+      .onSnapshot(querySnapshot => {
+        const onlineData = [];
+        querySnapshot.forEach(documentSnapshot => {
+          onlineData.push({
+            id: documentSnapshot.id,
+            ...documentSnapshot.data(),
+          });
+        });
+        setonline(onlineData);
+      });
+
+    return () => unsubscribe();
+  }, []);
+
+  const route = useRoute();
+  const {phoneNo} = route.params;
+
+  return (
+    <View>
+      <ImageBackground
+        resizeMode="cover"
+        style={styles.background}
+        source={require('../Images/background.jpg')}>
+        {online.length > 0 ? (
+          <View>
+            <View style={styles.FlatListVIew}>
+              <FlatList
+                data={online}
+                renderItem={({item}) => (
+                  <TouchableOpacity style={styles.DataView}>
+                    <View style={styles.DataView}>
+                      <Text allowFontScaling={false} style={styles.Name}>
+                        Name : {item.Name}
+                      </Text>
+                      <Text allowFontScaling={false} style={styles.Name}>
+                        Father Name : {item.Fathername}
+                      </Text>
+                      <Text allowFontScaling={false} style={styles.Name}>
                         Cousre : {item.CourseName}
+                      </Text>
+                      <Text allowFontScaling={false} style={styles.Name}>
+                        Response : {item.Response}
+                      </Text>
+                      <>
+                      {item.Response.toLowerCase() != 'pending' ? (
+                        <>
+                        <Text allowFontScaling={false} style={styles.Name}>
+                          Teacher: {item.Teacher}
                         </Text>
                         <Text allowFontScaling={false} style={styles.Name}>
-                        Response : {item.Response}
+                          Fees: {item.Fees}
                         </Text>
-                      </View>
-                    </TouchableOpacity>
-                  )}
-                  keyExtractor={item => item.id}
-                />
-              </View>
+                      </>
+                        ) : (
+                          console.log('Pending')
+                      )}
+                    </>                    
+                    </View>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={item => item.id}
+              />
             </View>
-          ) : (
-            <Text allowFontScaling={false} style={styles.NoData}>
-              No Data!!
-            </Text>
-          )}
-        </ImageBackground>
-      </View>
-    );
-  };
-  
-  export default UserData;
-  
-  const styles = StyleSheet.create({
-    background: {
-      width: devicewidth,
-      height: deviceheight,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  
-    FlatListVIew: {
-      width: responsiveWidth(90),
-    },
-    DataView: {
-      backgroundColor: '#2e4c60',
-      height: 'auto',
-      width: responsiveWidth(90),
-      marginVertical:responsiveHeight(1),
-      alignItems: 'center',
-      paddingVertical: responsiveHeight(0.75),
-      borderRadius: 12,
-    },
-  
-    Name: {
-      fontSize: responsiveScreenFontSize(2.25),
-      color: '#fff',
-      paddingVertical: responsiveHeight(1),
-      textAlign: 'center',
-      fontFamily: 'good',
-      letterSpacing: 2,
-    },
-    NoData: {
-      fontSize: responsiveScreenFontSize(4),
-      color: 'red',
-      textAlign: 'center',
-      fontFamily: 'good',
-      letterSpacing: 3,
-      textTransform: 'uppercase',
-    },
-  });
-  
+          </View>
+        ) : (
+          <Text allowFontScaling={false} style={styles.NoData}>
+            No Data!!
+          </Text>
+        )}
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default UserData;
+
+const styles = StyleSheet.create({
+  background: {
+    width: devicewidth,
+    height: deviceheight,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  FlatListVIew: {
+    width: responsiveWidth(90),
+  },
+  DataView: {
+    backgroundColor: '#2e4c60',
+    height: 'auto',
+    width: responsiveWidth(90),
+    marginVertical: responsiveHeight(1),
+    alignItems: 'center',
+    paddingVertical: responsiveHeight(0.75),
+    borderRadius: 12,
+  },
+
+  Name: {
+    fontSize: responsiveScreenFontSize(2.25),
+    color: '#fff',
+    paddingVertical: responsiveHeight(1),
+    textAlign: 'center',
+    fontFamily: 'good',
+    letterSpacing: 2,
+  },
+  NoData: {
+    fontSize: responsiveScreenFontSize(4),
+    color: 'red',
+    textAlign: 'center',
+    fontFamily: 'good',
+    letterSpacing: 3,
+    textTransform: 'uppercase',
+  },
+});
